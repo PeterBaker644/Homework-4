cde4ecc87b77005b2806701ae477965fb0c1b5f7
Blackstar <baker644@umn.edu>
2020-04-21 11:30:48 -0500

    Working read object write to html

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-var cardTitle = document.querySelector(".card-title")
-var cardQuestion = document.querySelector(".card-question")
-var cardFinish = document.querySelector(".card-finish")
-var cardHighscore = document.querySelector(".card-highscore")
+var cardTitle = document.querySelector(".card-title");
+var cardQuestion = document.querySelector(".card-question");
+var cardFinish = document.querySelector(".card-finish");
+var cardHighscore = document.querySelector(".card-highscore");
 
-var startQuiz = document.querySelector("#start-quiz")
-var highScores = document.querySelector(".highscores")
-var submitInit = document.querySelector("#submit-init")
-var back = document.querySelector("#back")
-var resetScore = document.querySelector("#reset-score")
+var startQuiz = document.querySelector("#start-quiz");
+var highScores = document.querySelector(".highscores");
+var submitInit = document.querySelector("#submit-init");
+var back = document.querySelector("#back");
+var resetScore = document.querySelector("#reset-score");
+var questionTitle = document.querySelector("#question-title");
+var questionList = document.querySelector("#question-list");
 var inGame = false;
+var currentQuestion = 1;
 
 var questions = {
     "1": {
         "question": "This is a sample question? This is a really long sample question that is going to wrap at least once.",
-        "answers": {
-            "1": "This is the first answer.",
-            "2": "This is the second answer to the question.",
-            "3": "3",
-            "4": "Last Answer.",
-        },
+        "answers": [
+            "This is the first answer.",
+            "This is the second answer to the question.",
+            "3",
+            "Last Answer."
+        ],
         "correctAnswer": "4" 
       }
+}
+
+function setQuestion() {
+    questionTitle.textContent = questions[currentQuestion].question
+    for (var value of questions[currentQuestion].answers) {
+        var li = document.createElement("li");
+        li.textContent = value;
+        questionList.appendChild(li);
+    }
 }
 
 
@@ -40,6 +52,7 @@ startQuiz.addEventListener("click", function () {
     hide(cardTitle);
     show(cardQuestion);
     // startGame();
+    setQuestion();
 });
 
 highScores.addEventListener("click", function () {
